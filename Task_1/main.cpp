d68da306c59542a20899d5e920f8ccbb0345b9c7
dichter-raugh <d.romskiy@mail.ru>
2020-04-16 21:07:53 +0700

    add function that count phrases and first tests

--- Task_1/main.cpp
+++ Task_1/main.cpp
@@ -1,5 +1,42 @@
-#include <iostream>
+#include"phrases.h"
 
-int main() {
-	std::cout << "Goodbye World" << std::endl;
+int main(int argc, char* argv[]) 
+{
+	int i,n = 2,m = 2;
+	std::string argument, filename;
+	std::map<std::string, int> phrases;
+	std::ifstream file;
+	std::map<std::string, int>::iterator it;
+	for (i = 0; i < argc; i++)
+	{
+		argument = argv[i];
+		if (argument.compare("-n"))
+		{
+			n = (int)argv[i + 1];
+		}
+		if (argument.compare("-m"))
+		{
+			m = (int)argv[i + 1];
+		}
+		if (argument.find(".txt") != std::string::npos)
+		{
+			filename = argument;
+		}
+	}
+	if (filename.empty())
+	{
+		PhrasesFromFile(n, phrases, std::cin);
+	}
+	else
+	{
+		file.open(filename);
+		PhrasesFromFile(n, phrases, file);
+		file.close();
+	}
+	for (it = phrases.begin(); it != phrases.end(); ++it)
+	{
+		if (it->second >= m)
+			std::cout << it->first << "(" << it->second << ")" << std::endl;
+	}
 }
+
